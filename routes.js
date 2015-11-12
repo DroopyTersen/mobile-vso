2be0e21cb99d964511906b07288bc3a7564b69cf
Andrew Petersen <andrew.petersen15@gmail.com>
2015-11-12 00:29:01 -0600

    got login working

--- routes.js
+++ routes.js
@@ -1,18 +1,25 @@
 var createRoutes = function(app, passport) {
     
-    app.get("/", function(req, res) {
+    app.get("/", authorize, function(req, res) {
         res.sendFile(__dirname + '/client/index.html')
     })
+    
+        
+    app.get("/login", function(req, res) {
+        res.sendFile(__dirname + '/client/login.html')
+    })
+    
+    var redirects = { successRedirect: "/", failureRedirect: "/login" }
+    app.post('/login', passport.authenticate('local', redirects));
     
     app.get("/signout", (req, res) => {
         req.logout();
         res.redirect("/login");
     });
     
-    var authorize = function(req, res, next) {
-        if (req.isAuthenticated()) return next(req, res);
-        
-        res.redirect("/login")
+    
+    function authorize(req, res, next) {
+        return req.isAuthenticated() ? next() : res.redirect("/login");
     }
 }
 
